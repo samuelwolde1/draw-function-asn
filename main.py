90d5a1280f2d57b1f224b76b45d2dda94e38477f
samuelwolde1 <126281252+samuelwolde1@users.noreply.github.com>
2024-02-08 10:35:34 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -5,13 +5,18 @@ def drawRect(color1, color2, x, y, width, height):
     pygame.draw.rect(screen, color1, [x, y, width, height])
     pygame.draw.rect(screen, color2, [x, y - height, width, height])
 
+#function to draw target
+def drawTarget(color, x, y, radius, rings):
+    for ring_num in range(1, rings + 1):
+        dist = radius / rings
+        pygame.draw.circle(screen, color, (x, y), dist * ring_num, 1)
+
 #Define colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-GREEN = (0, 255,   0)
+GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
-
 
 #Screen settings
 size = (700, 500)
@@ -35,7 +40,9 @@ while not done:
     drawRect(BLACK, BLUE, 100, 100, 200, 10)
     drawRect(GREEN, RED, 200, 200, 300, 50)
     drawRect(BLUE, RED, 300, 300, 200, 10)  
-
+    drawTarget(BLACK, 150, 150, 100, 5)
+    drawTarget(BLUE, 400, 300, 100, 20)
+    drawTarget(GREEN, 150, 400, 100, 10)
     pygame.display.flip()
     clock.tick(60)    
     
